46d0f13c4e188f909c233f6d14eeba4fbbcff783
Vinícius Gabriel Leitempergher <viniciusgabriel.com@gmail.com>
2021-07-09 12:43:13 -0300

    Editar Turmas Finalizado.

--- pages/turmas/gerenciaTurma.js
+++ pages/turmas/gerenciaTurma.js
@@ -2,7 +2,7 @@ import popup from '../popup/modal.js'
 import { Turma } from './turma.js'
 
 document.querySelector("#adicionarbtn").addEventListener("click", adicionarTurma)
-document.querySelector("#editarbtn").addEventListener("click", selectTurma)
+document.querySelector("#editarbtn").addEventListener("click", () => selectTurma("Clique na turma para editar", editarTurma))
 document.querySelector("#removerbtn").addEventListener("click", removerTurma)
 
 let turmas = []
@@ -44,7 +44,7 @@ window.addEventListener("load", () => {
     atualizarTurmas()
 })
 
-function selectTurma() {
+function selectTurma(txt, callback) {
     for (let botao of document.querySelectorAll(".main__wrapper__menu__button")) {
         botao.style.display = "none"
     }
@@ -56,25 +56,60 @@ function selectTurma() {
     cancelAction.textContent = "Cancelar"
 
     let texto = document.createElement("h3")
-    texto.textContent = "Clique na sala que quer alterar"
+    texto.textContent = txt
+
+    let callbackComCodigo = function(e) {
+        let hashTagsSplitted = e.target.textContent.split("#")
+        callback(hashTagsSplitted[hashTagsSplitted.length - 1])
+        voltaAoNormal()
+        for (let turma of document.querySelectorAll(".turma__wrapper")) {
+            turma.removeEventListener("click", callbackComCodigo)
+        }
+    }
+
+    for (let turma of document.querySelectorAll(".turma__wrapper")) {
+        turma.addEventListener("click", callbackComCodigo)
+    }
+
+    cancelAction.addEventListener("click", () => {
+        voltaAoNormal()
+    })
+
+    // Sai do modo de edição e mostra os botões de novo.
+    function voltaAoNormal() {
+        cancelAction.remove()
+        texto.remove()
+        for (let botao of document.querySelectorAll(".main__wrapper__menu__button")) {
+            botao.style.display = ""
+        }
+    }
 
     menu.prepend(cancelAction)
     menu.prepend(texto)
 }
 
-function editarTurma() {
-
+function editarTurma(codigo) {
     popup(`
     <h2 class="modalpopup__h2">Editar Turma:</h2>
     <input type="text" class="modalpopup__form__input" id="input__nometurma" placeholder="Nome da Turma">
     `)
-    let form = document.querySelector(".modalpopup__form")
+    let form = document.querySelector(".modalpopup__form"),
+        cancel = document.querySelectorAll(".modalpopup__form__button")[1]
 
     form.addEventListener("submit", (e) => {
-        nome = document.querySelector("#input__nometurma").value
-        document.body.removeChild(document.querySelector(".modalpopup"))
+        let nome = document.querySelector("#input__nometurma").value
+        document.querySelector(".modalpopup").remove()
 
-        
+        turmas = getTurmas()
+
+        let index = 0
+        for (let turma of turmas) {
+            if (turma.codigo == codigo) {
+                turma.editarTurma(nome)
+                turmas[index] = turma
+            }
+            index++
+        }
 
         localStorage.setItem("turmas", JSON.stringify(turmas))
         atualizarTurmas()
@@ -84,6 +119,10 @@ function editarTurma() {
         if (e.key == "Escape") {
             cancel.click()
         }
+    })
+    cancel.addEventListener("click", (e) => {
+        e.preventDefault()
+        document.querySelector(".modalpopup").remove()
     })
 }
 
@@ -116,4 +155,17 @@ function atualizarTurmas() {
         </div>
         `)
     }
+}
+
+// Pega as informações da memória e retorna elas num objeto do tipo Turma
+function getTurmas() {
+    let turmas = []
+    let turmasWithoutType = JSON.parse(localStorage.getItem("turmas"))
+
+    for (let turmaWithoutType of turmasWithoutType) {
+        let turma = new Turma(turmaWithoutType.codigo, turmaWithoutType.nome)
+        turmas.push(turma)
+    }
+
+    return turmas
 }
