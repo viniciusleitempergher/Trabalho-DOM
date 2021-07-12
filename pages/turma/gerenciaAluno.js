4fa1bfed1b2d8ad1138cc55da80d6e54b5c3bdb9
Vinícius Gabriel Leitempergher <viniciusgabriel.com@gmail.com>
2021-07-11 23:39:59 -0300

    Cadastrar notas finalizado

--- pages/turma/gerenciaAluno.js
+++ pages/turma/gerenciaAluno.js
@@ -4,7 +4,7 @@ import popup from '../popup/modal.js'
 
 document.querySelector("#adicionarbtn").addEventListener("click", adicionarAluno)
 document.querySelector("#cadastrarbtn").addEventListener("click", (e) => {
-  selecionarAluno("Clique no aluno para cadastrar", console.log)
+  selecionarAluno("Clique no aluno para cadastrar", cadastrarNotas)
 })
 
 const queryString = new URLSearchParams(location.search)
@@ -32,7 +32,9 @@ function getTurma() {
       let turma = new Turma(turmaWithoutType.codigo, turmaWithoutType.nome)
 
       for (let aluno of turmaWithoutType.alunos) {
-        turma.cadastrarAluno(new Aluno(aluno.matricula, aluno.nome, aluno.telefone, aluno.email))
+        let alunoTipado = new Aluno(aluno.matricula, aluno.nome, aluno.telefone, aluno.email)
+        alunoTipado.notas = aluno.notas
+        turma.cadastrarAluno(alunoTipado)
       }
 
       return turma
@@ -43,6 +45,7 @@ function getTurma() {
 }
 
 function atualizarAlunos(pesquisa, matricula) {
+  turma = getTurma()
   alunos = turma.alunos
 
   let listaAlunos = document.querySelector(".main__wrapper__alunos")
@@ -61,8 +64,8 @@ function atualizarAlunos(pesquisa, matricula) {
             <p class="alunos__info">${aluno.nome}</p>
             <p class="alunos__info">${aluno.telefone}</p>
             <p class="alunos__info">${aluno.email}</p>
-            <p class="alunos__info">${aluno.notas}</p>
-            <p class="alunos__info">${aluno.calculaMedia() || ""}</p>
+            <p class="alunos__info">${Array.from(aluno.notas) || ""}</p>
+            <p class="alunos__info">${aluno.calculaMedia() || ((aluno.calculaMedia() == 0) ? "0" : "")}</p>
       `)
     }
   }
@@ -167,4 +170,51 @@ function selecionarAluno(txt, callback) {
 
   menu.prepend(cancelAction)
   menu.prepend(texto)
+}
+
+function cadastrarNotas(matricula) {
+  popup(`
+    <h2 class="modalpopup__h2">Cadastrar Notas:</h2>
+    <input type="text" class="modalpopup__form__input" id="input-nota1" placeholder="Nota 1">
+    <input type="text" class="modalpopup__form__input" id="input-nota2" placeholder="Nota 2">
+    <input type="text" class="modalpopup__form__input" id="input-nota3" placeholder="Nota 3">
+  `)
+
+  let form = document.querySelector(".modalpopup__form")
+  let cancel = document.querySelectorAll(".modalpopup__form__button")[1]
+
+  cancel.addEventListener("click", (e) => {
+    e.preventDefault()
+    document.querySelector(".modalpopup").remove()
+  })
+  form.addEventListener("submit", (e) => {
+    let nota1 = document.querySelector("#input-nota1").value,
+        nota2 = document.querySelector("#input-nota2").value,
+        nota3 = document.querySelector("#input-nota3").value
+
+    document.querySelector(".modalpopup").remove()
+
+    let turmas = JSON.parse(localStorage.getItem("turmas"))
+
+    for (let i = 0; i < turmas.length; i++) {
+      if (turmas[i].codigo == turma.codigo) {
+        for (let aluno of turmas[i].alunos) {
+          if (aluno.matricula == matricula) {
+            aluno.notas = [+nota1, +nota2, +nota3]
+            break
+          }
+        }
+        break
+      }
+    }
+
+    localStorage.setItem("turmas", JSON.stringify(turmas))
+    atualizarAlunos("", "")
+    e.preventDefault()
+  })
+  form.addEventListener("keydown", (e) => {
+    if (e.key == "Escape") {
+      cancel.click()
+    }
+  })
 }
